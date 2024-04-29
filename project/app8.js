57f09e97c6996750bc55eb2cf78884b3a0721625
fai-sal-786 <154748475+fai-sal-786@users.noreply.github.com>
2024-04-29 13:00:14 -0700

    strings concatenation

---
+++ project/app8.js
@@ -0,0 +1,58 @@
+// Q1:create a variable first name and assign it the value' john 'then concatenate it with the string 'doe' and display the fullname.?
+
+
+// let first_name = 'John';
+// let full_name = first_name.trim() + 'Doe';
+// console.log("Full name:", full_name);
+
+// Q2:Declare a variable city and set it to 'NEW'. Next, concatenate it with the string 'york' and display the complete  city name?
+
+// let city ='NEW';
+// let complete_city = city + 'york';
+// console.log(complete_city);
+
+// Q3: Define a variable fruit with the value 'apple'. concatenate it with the string 'pie' to form a dessert and then print it?
+
+// let fruit = 'APPLE';
+// let dessert = fruit + 'pie';
+// console.log("dessert", dessert);
+
+// Q4: initialize a variable animal with ' black' . Concatenate it with the string 'bird' and display the full animal name.?
+
+// let animal = 'black';
+// let full_animal_name = animal.trim() + 'bird';
+// console.log("full animal name:", full_animal_name);
+
+// Q5: COMBINE THE STRING . 'Javascript' and 'is fun' to create a sentences, then display it?
+
+// let sentence = 'javascript' + 'is fun';
+// console.log("Sentence:", sentence);
+
+// Q6:Concatenate 'open' with 'source' and 'intelligence' to form a technology companys name and show it.?
+
+// let company_name = 'open' + 'source' + 'intelligence';
+// console.log("Company NAME: ", company_name);
+
+// Q7: join the strings 'happy' 'birthday' and 'to you'  to wish someone on their special DynamicsCompressorNode,  and then display the greeting?
+
+// let greeting = ['Happy', 'Birthday', 'To You'].join("");
+// console.log("BIRTHDAY GREETING:", greeting);
+
+// Q8:Concatenate 'GOOD' with 'LUCK' and 'HAVE' with 'fun' to create well wishes, and then display both phrases?
+
+// let phrases1 = 'GOOD' + 'LUCK';
+// let phrases2 = 'HAVE' + 'FUN';
+
+// console.log('phrases1:', phrases1);
+// console.log('phrases2:', phrases2);
+
+// Q9: COMBINE THE STRINGS:
+// 'web', 'Design', and 'Devolpment' to describe a comprehensive skill set and then print it?
+
+// let skill_set = 'web' + 'design' + 'devolpement';
+// console.log('skill set:', skill_set);
+
+// Q10: JOIN 'ARTIFICAL' 'NEUTRAL' and  'networks' to form a complex technology term and then display it?
+
+// let complex_term =['ARTIFICAL' + 'NEUTRAL' + 'NETWORKS'].join('');
+// console.log("complex technology term:", complex_term);
